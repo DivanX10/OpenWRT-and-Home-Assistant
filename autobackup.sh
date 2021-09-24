8df9bf307c223f7a790c0655581b50ab84e92d80
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2021-09-24 18:31:50 +0300

    Update autobackup.sh

--- autobackup.sh
+++ autobackup.sh
@@ -2,4 +2,5 @@
 echo "Starting the backup of the Home Assistant, zigbee2mqtt, mosquitto, mpd"
 sleep 2
 mkdir /backup&&
-tar cvf /backup/gateway_backup-$(date +%Y%m%d).tar /etc/mpd.conf /etc/homeassistant /etc/mosquitto /etc/zigbee2mqtt
+tar cvf /backup/gateway_backup-$(date +%Y%m%d).tar /etc/mpd.conf /etc/homeassistant /etc/mosquitto /etc/zigbee2mqtt;
+echo "Done"
