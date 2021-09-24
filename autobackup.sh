9c301f7b09bc7bb9dc93a566dbc8d5777216a59e
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2021-09-24 22:07:59 +0300

    Update autobackup.sh

--- autobackup.sh
+++ autobackup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
-# backup - указываем путь, где будет храниться бэкап
 backup="/backup/gateway_backup-$(date +%Y%m%d).tar"
 
-# bkpfolder - бэкап папок. Указываем какие папки нужно бэкапить
-bkpfolder="/etc/homeassistant
+bkpfolder="
+/etc/homeassistant
 /etc/zigbee2mqtt
 /etc/mosquitto
 /mpd"
 
-# bkpfile - бэкап файлов. Указываем какие файлы нужно бэкапить
 bkpfile="/etc/mpd.conf"
 
 echo "Starting a backup"
