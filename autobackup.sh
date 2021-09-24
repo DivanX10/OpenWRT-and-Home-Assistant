985f8129594715986430720b5a99f31f021dfd21
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2021-09-24 16:35:58 +0300

    Update autobackup.sh

--- autobackup.sh
+++ autobackup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-tar -cvf /backup/gateway_backup-$(date +%Y%m%d).tar /etc/homeassistant /etc/zigbee2mqtt /etc/mosquitto /mpd
+echo "Starting the backup of the Home Assistant, zigbee2mqtt, mosquitto, mpd"
+sleep 2
+mkdir backup&&
+sleep 2
+wget https://github.com/DivanX10/OpenWRT-and-Home-Assistant/edit/main/autobackup.sh -O /tmp/autobackup.sh&&
+sleep 2
+tar -cvf /backup/gateway_backup-$(date +%Y%m%d).tar /etc/homeassistant /etc/zigbee2mqtt /etc/mosquitto /mpd&&
