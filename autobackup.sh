9f1295ad4469bae663528ff56cc94006dbf151b4
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2021-09-24 16:21:21 +0300

    Update autobackup.sh

--- autobackup.sh
+++ autobackup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tar -cvf /backup/gateway_backup-$(date +%Y%m%d).tar /etc/homeassistant /etc/zigbee2mqtt /etc/mosquitto /mpd /scripts
+tar -cvf /backup/gateway_backup-$(date +%Y%m%d).tar /etc/homeassistant /etc/zigbee2mqtt /etc/mosquitto /mpd
