50adec91110a51dbcfec51d5de1b441fe374fc05
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2021-09-24 22:59:12 +0300

    Update copying_and_starting_backup.sh

--- copying_and_starting_backup.sh
+++ copying_and_starting_backup.sh
@@ -2,5 +2,6 @@
 mkdir /backup&&
 mkdir /scripts&&
 wget https://raw.githubusercontent.com/DivanX10/OpenWRT-and-Home-Assistant/main/autobackup.sh -O /scripts/autobackup.sh&&
+sleep1
 chmod ugo+x /scripts/autobackup.sh&&
 sh /scripts/autobackup.sh
