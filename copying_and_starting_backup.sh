463750546345381b6d1319a6cb537a9d000e9f15
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2021-09-24 23:09:46 +0300

    Delete copying_and_starting_backup.sh

--- copying_and_starting_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir /backup&&
-mkdir /scripts&&
-wget https://raw.githubusercontent.com/DivanX10/OpenWRT-and-Home-Assistant/main/autobackup.sh -O /scripts/autobackup.sh&&
-sleep1
-chmod ugo+x /scripts/autobackup.sh&&
-sh /scripts/autobackup.sh
