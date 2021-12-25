d10cf2190d6b56c526d6ba425acc211d61d9841a
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2021-12-26 01:02:49 +0300

    Update homeassistant_python3.9_delete.sh

--- scripts/homeassistant_python3.9_delete.sh
+++ scripts/homeassistant_python3.9_delete.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+set -e
 echo "Starting the Home Assistant removal"
 sleep 1
-/etc/init.d/homeassistant stop&&
-sleep 5
-pip3 uninstall homeassistant -y&&
-rm -R /etc/homeassistant&&
-rm -R /usr/lib/python3.9/site-packages/hass_frontend*&&
-rm -R /usr/lib/python3.9/site-packages/hass_nabucasa*&&
-rm -R /usr/lib/python3.9/site-packages/home_assistant_frontend*&&
-rm -R /root/.homeassistant&&
-rm -R /etc/init.d/homeassistant&&
+/etc/init.d/homeassistant stop
+sleep 20
+pip3 uninstall homeassistant -y
+rm -R /etc/homeassistant
+rm -R /usr/lib/python3.9/site-packages/hass_frontend*
+rm -R /usr/lib/python3.9/site-packages/hass_nabucasa*
+rm -R /usr/lib/python3.9/site-packages/home_assistant_frontend*
+rm -R /root/.homeassistant
+rm -R /etc/init.d/homeassistant
 echo "Home Assistant was successfully deleted"
 exit
