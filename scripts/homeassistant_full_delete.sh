348f18a437c31a9f188ca428a39a812d229e7539
DivanX10 <64090632+DivanX10@users.noreply.github.com>
2021-12-26 20:34:17 +0300

    Update homeassistant_full_delete.sh

--- scripts/homeassistant_full_delete.sh
+++ scripts/homeassistant_full_delete.sh
@@ -3,7 +3,7 @@ set -e
 echo "Starting the Home Assistant removal"
 /etc/init.d/homeassistant stop
 /etc/init.d/homeassistant disable
-wget https://raw.githubusercontent.com/DivanX10/OpenWRT-and-Home-Assistant/main/requirements.txt -O /tmp/requirements.txt
+wget https://raw.githubusercontent.com/DivanX10/OpenWRT-and-Home-Assistant/main/files/requirements_all.txt -O /tmp/requirements_all.txt
 pip3 uninstall homeassistant -y
 rm -R /etc/homeassistant
 rm -R /usr/lib/python3.9/site-packages/hass_frontend*
@@ -13,7 +13,7 @@ rm -R /root/.homeassistant
 rm -R /etc/init.d/homeassistant
 echo "Home Assistant was successfully deleted"
 sleep 2
-pip uninstall -r /tmp/requirements.txt -y
+python -m pip uninstall -r -y /tmp/requirements_all.txt
 echo "The dependent packages have been removed. Home Assistant has been completely removed"
 sleep 2
 exit
